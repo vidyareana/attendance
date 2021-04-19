4142ff846920ece789f60b9bfe5d13ba8af13f84
Vidya ramadani <vidyavio2412@gmail.com>
2021-04-20 04:18:09 +0700

    hallo

--- sendmaill.php
+++ sendmaill.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
 $from = "vidya@gmail.com";
@@ -8,4 +8,4 @@ $message = "Registrasi berhasil";
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers);
 echo "The email message was sent.";
-?>
+?> -->
